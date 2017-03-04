063150f2a14b39054cf6fc2386bd51db7abb353f
Daniel Galasko <danielhgalasko@gmail.com>
2017-03-04 09:16:04 +0200

    Initial Commit

---
+++ AfrikaBurn/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  AfrikaBurn
+//
+//  Created by Daniel Galasko on 2017/03/04.
+//  Copyright © 2017 AfrikaBurn. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
