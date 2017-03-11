880448503e880b20a072b640c706a247c7828d09
Daniel Galasko <danielhgalasko@gmail.com>
2017-03-11 15:00:42 +0200

    added ability to filter by type

--- AfrikaBurn/FirstViewController.swift
+++ AfrikaBurn/FirstViewController.swift
@@ -24,7 +24,17 @@ class FirstViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
-    var camps: [Camp] = []
+    var camps: [Camp] = [] {
+        didSet {
+            self.tableView.reloadData()
+        }
+    }
+    var allCamps: [Camp] = []
+    
+    lazy var types: Set<String> = {
+        let types: Set<String> = Set(self.camps.map { $0.type })
+        return types
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,6 +42,19 @@ class FirstViewController: UIViewController {
         tableView.delegate = self
         tableView.enableSelfSizingCells(withEstimatedHeight: 55)
         camps = ThemeCampCSVParser().parseSync()
+        allCamps = camps
+    }
+    @IBAction func handleFilterTapped(_ sender: Any) {
+        let actionSheet = UIAlertController(title: "Filter", message: "choose a type", preferredStyle: .actionSheet)
+        for type in types {
+            actionSheet.addAction(UIAlertAction(title: type, style: .default, handler: { _ in
+                self.camps = self.allCamps.filter({ $0.type == type })
+            }))
+        }
+        actionSheet.addAction(UIAlertAction(title: "Reset", style: .default, handler: { _ in
+            self.camps = self.allCamps
+        }))
+        present(actionSheet, animated: true, completion: nil)
     }
 }
 
