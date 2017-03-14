66ac07e639a93e2f963a8351052f9835063c2aae
Daniel Galasko <danielhgalasko@gmail.com>
2017-03-14 07:57:33 +0200

    remove elements missing from the JSON when saving

--- AfrikaBurn/Model/PersistentStore.swift
+++ AfrikaBurn/Model/PersistentStore.swift
@@ -14,10 +14,20 @@ class PersistentStore {
         return try! Realm()
     }
     
-    func storeElements(_ elements: [AfrikaBurnElement]) {
+    func storeElements(_ elements: [AfrikaBurnElement], deleteRestNotIncludedInElements: Bool = true) {
         DispatchQueue.global(qos: .background).async {
             let realm = self.createRealm()
+            /*
+             let context = Element.Context.dailyElement
+             let idsToSave = elements.map { $0.id }
+             db.delete(Element.self, matchingPredicate: NSPredicate(format: "NOT id IN %@", argumentArray: [idsToSave]), context: context)
+             */
             try! realm.write {
+                if deleteRestNotIncludedInElements {
+                    let idsToSave = elements.map({ $0.id })
+                    let toDelete = realm.objects(AfrikaBurnElement.self).filter("NOT id in %@", idsToSave)
+                    realm.delete(toDelete)
+                }
                 realm.add(elements, update: true)
             }
         }
