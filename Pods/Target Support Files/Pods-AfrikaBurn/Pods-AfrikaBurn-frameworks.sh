423b785c20cc8160028b00d51875bd7768d21a58
Daniel Galasko <danielhgalasko@gmail.com>
2018-03-26 15:38:26 +0200

    updated realm and removed CHCSVParser

--- Pods/Target Support Files/Pods-AfrikaBurn/Pods-AfrikaBurn-frameworks.sh
+++ Pods/Target Support Files/Pods-AfrikaBurn/Pods-AfrikaBurn-frameworks.sh
@@ -134,12 +134,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/CHCSVParser/CHCSVParser.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Realm/Realm.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RealmSwift/RealmSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/CHCSVParser/CHCSVParser.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Realm/Realm.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RealmSwift/RealmSwift.framework"
 fi
